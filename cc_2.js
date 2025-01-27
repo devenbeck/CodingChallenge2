61b01a4331ef2e149359b0a2af9d471d38e3ae2e
Natalie Devenbeck <devenbeck@usf.edu>
2025-01-26 21:27:13 -0500

    Task 1- Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,7 @@
+// Working with Arrays//
+let products = ["Nike sneaker","Adidas shoe", "Reebok shoe", "Hoka Running shoe", "Asics sneaker"] //Array of 5 different products
+products.push("Brooks trainers"); //add a new product to the end of the array
+console.log(products); //display the array of products
+
+products.pop(); //removes the last product from the array
+console.log("Updated Product List:", products); //display the final updated list
